98cf2a6164ef5d7df545b299d49bfbec32e43d2e
Aleezhka <151876861+Aleezhka@users.noreply.github.com>
2024-12-01 23:14:18 +0200

    Лабораторна робота 12

--- Lab_10/my-app/src/components/PrimaryButton/PrimaryButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ButtonWrapper } from './PrimaryButton.styled';
-
-const PrimaryButton = ({ onClick, children }) => (
-    <ButtonWrapper onClick={onClick}>{children}</ButtonWrapper>
-);
-
-export default PrimaryButton;
--- Lab_10/my-app/src/components/Select/Select.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const SelectWrapper = styled.select`
-  padding: 8px 12px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  margin: 10px 0;
-`;
